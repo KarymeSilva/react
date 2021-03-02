4a0a09f97da5b636c552f05830721915a6ee5ce4
KarymeSilva <karyme.silva.279@gmail.com>
2021-03-02 15:37:42 -0600

    commitcuatro

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {Helmet} from "react-helmet";
 function App() {
   return (
     <div className="App">
-      <h1>Google Maps App</h1>
+      <h1>Google Maps App api</h1>
       <GoogleMap />
       <Helmet>
       <script defer data-key="b0fbfacf-03cb-495c-9bef-b12f85e4ece6" src="https://widget.tochat.be/bundle.js"></script>
