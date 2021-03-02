a2fcb8a367384f1b7a72146362ade48f6c06c20b
KarymeSilva <karyme.silva.279@gmail.com>
2021-03-02 15:42:20 -0600

    commitcinco

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Google Maps App api</h1>
+      <h2>Bienvenido a la API MAPS</h2>
       <GoogleMap />
       <Helmet>
       <script defer data-key="b0fbfacf-03cb-495c-9bef-b12f85e4ece6" src="https://widget.tochat.be/bundle.js"></script>
