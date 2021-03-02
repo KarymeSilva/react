dd14777b57480e34e2df42c044e25a993288ca31
KarymeSilva <karyme.silva.279@gmail.com>
2021-03-02 15:29:25 -0600

    committres

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import GoogleMap from './components/GoogleMap';
+import {Helmet} from "react-helmet";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Google Maps App</h1>
+      <GoogleMap />
+      <Helmet>
+      <script defer data-key="b0fbfacf-03cb-495c-9bef-b12f85e4ece6" src="https://widget.tochat.be/bundle.js"></script>
+       </Helmet>
+
+    </div>
+  );
+}
+
+export default App;
